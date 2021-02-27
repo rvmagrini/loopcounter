486bbd0024d654cf93558e4ad797915e185fa342
Renato Magrini <renatomagrini@gmail.com>
2021-02-27 12:28:14 +0100

    empty fields bug fixed

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-const load = () => {
-  const now = new Date().getHours();
-  // const now = 10;
+const count = () => {
+  // Tagging html fields
 
-  const msg = document.querySelector("#msg");
-  const pic = document.querySelector("#pic");
+  let txtStart = document.querySelector("#txtstart").value;
+  let txtFinish = document.querySelector("#txtfinish").value;
+  let txtStep = document.querySelector("#txtstep").value;
+  let outcome = document.querySelector("#outcome");
 
-  msg.innerHTML = `Current Local Time: ${now}h.`;
-
-  if (now < 6) {
-    msg.innerHTML += `<p>Good Night...</p>`;
-    pic.src = "../img/night.png";
-    document.body.style.background = "#798EA2";
-  } else if (now < 12) {
-    msg.innerHTML += `<p>Good Morning!</p>`;
-    pic.src = "../img/morning.png";
-    document.body.style.background = "#ffee93";
-  } else if (now < 18) {
-    msg.innerHTML += `<p>Good Afternoon!</p>`;
-    pic.src = "../img/afternoon.png";
-    document.body.style.background = "#a1cae2";
+  // Checkin fields
+  if (txtStart.length === 0 || txtFinish.length === 0 || txtStep.length === 0) {
+    outcome.innerHTML = `Please insert the data correctly.`;
   } else {
-    msg.innerHTML += `<p>Good Evening!</p>`;
-    pic.src = "../img/night.png";
-    document.body.style.background = "#798EA2";
+    outcome.innerHTML = `<h3>Contando...</h3>`;
+
+    // Converting fields to numbers
+    let startNum = Number(txtStart);
+    let finishNum = Number(txtFinish);
+    let step = Number(txtStep);
+
+    //
+    if (step === 0) {
+      step = 1;
+    }
+
+    if (startNum >= 1000000) {
+      alert("Starting number can not be greater than 1000000.");
+    }
+
+    // Looping and Printing
+
+    if (startNum < finishNum) {
+      for (let num = startNum; num <= finishNum; num += step) {
+        outcome.innerHTML += ` ${num} \u{1F449} `;
+      }
+    } else {
+      for (let num = startNum; num >= finishNum; num -= step) {
+        outcome.innerHTML += ` ${num} \u{1f449}`;
+      }
+    }
+    outcome.innerHTML += `\u{1F3C6}`;
   }
 };
