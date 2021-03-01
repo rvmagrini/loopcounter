f7a9db655c0896165545ee6708e750f4b5720923
Renato Magrini <renatomagrini@gmail.com>
2021-03-01 23:14:24 +0100

    focus and clear fields

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const count = () => {
   // Tagging html fields
 
-  let txtStart = document.querySelector("#txtstart").value;
-  let txtFinish = document.querySelector("#txtfinish").value;
-  let txtStep = document.querySelector("#txtstep").value;
+  let txtStart = document.querySelector("#txtstart");
+  let txtFinish = document.querySelector("#txtfinish");
+  let txtStep = document.querySelector("#txtstep");
   let outcome = document.querySelector("#outcome");
 
   // Checkin fields
-  if (txtStart.length === 0 || txtFinish.length === 0 || txtStep.length === 0) {
+  if (
+    txtStart.value.length === 0 ||
+    txtFinish.value.length === 0 ||
+    txtStep.value.length === 0
+  ) {
     outcome.innerHTML = `Please insert the data correctly.`;
   } else {
     outcome.innerHTML = `<h3>Contando...</h3>`;
 
     // Converting fields to numbers
-    let startNum = Number(txtStart);
-    let finishNum = Number(txtFinish);
-    let step = Number(txtStep);
+    let startNum = Number(txtStart.value);
+    let finishNum = Number(txtFinish.value);
+    let step = Number(txtStep.value);
 
-    //
+    // Filtering
     if (step === 0) {
       step = 1;
       alert("Zero is not a valid step. Step will be considered as 1.");
@@ -38,6 +42,12 @@ const count = () => {
         outcome.innerHTML += ` ${num} \u{1f449}`;
       }
     }
+
     outcome.innerHTML += `\u{1F3C6}`;
+
+    txtStart.value = "";
+    txtFinish.value = "";
+    txtStep.value = "";
+    txtStart.focus();
   }
 };
